5f5017a373707ef0fde163fbb44132e4d9d2a615
William Nollert <will.nollert@gmail.com>
2022-02-15 18:44:30 -0600

    created users table

--- db/init_db.js
+++ db/init_db.js
@@ -2,15 +2,29 @@ const {
   client,
   // declare your model imports here
   // for example, User
-} = require('./');
+} = require("./");
 
 async function buildTables() {
   try {
     client.connect();
-
     // drop tables in correct order
 
     // build tables in correct order
+
+    /* this starts the build tables query i'm not sure
+    if createTables and dropTables need to be seperate 
+    async fucntions inside of  this buildTables  function 
+    or if the are okay as is   */
+    await client.query(`
+    CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        first_name TEXT NOT NULL,
+        email  VARCHAR(255),
+        phone INTEGER,
+    );
+    `);
   } catch (error) {
     throw error;
   }
