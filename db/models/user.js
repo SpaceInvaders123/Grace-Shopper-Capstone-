6c6767a8b33e54aecd80078d9bdbe1f4ca8a2874
William Nollert <will.nollert@gmail.com>
2022-02-16 10:12:11 -0600

    added getAllUsers db adapter

--- db/models/user.js
+++ db/models/user.js
@@ -1,5 +1,5 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
+const client = require("../client");
 
 module.exports = {
   // add your database adapter fns here
@@ -8,4 +8,13 @@ module.exports = {
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
+  try {
+    const { rows: users } = await client.query(`
+      SELECT * FROM users
+      `);
+
+    return users;
+  } catch (error) {
+    throw error;
+  }
 }
