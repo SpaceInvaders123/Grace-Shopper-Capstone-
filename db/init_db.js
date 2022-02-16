cdc44c9417d19ae81d8d6022d27efb8e2c7757fa
William Nollert <will.nollert@gmail.com>
2022-02-16 12:07:31 -0600

    added create/drop table functions with Users table

--- db/init_db.js
+++ db/init_db.js
@@ -2,15 +2,34 @@ const {
   client,
   // declare your model imports here
   // for example, User
-} = require('./');
+} = require("./");
+
+async function dropTables() {
+  await client.query(`
+  DROP TABLES IF EXISTS users;
+  `);
+}
+
+async function createTables() {
+  await client.query(`
+   CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        first_name TEXT NOT NULL,
+        email  VARCHAR(255),
+        phone INTEGER,
+    );
+  `);
+}
 
 async function buildTables() {
   try {
     client.connect();
-
     // drop tables in correct order
-
+    await dropTables();
     // build tables in correct order
+    await createTables();
   } catch (error) {
     throw error;
   }
