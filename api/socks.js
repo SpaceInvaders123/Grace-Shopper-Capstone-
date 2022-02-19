d2520f7100c7fcb83a54cb6db5435faf009a748f
erikalee <slperika@hotmail.com>
2022-02-19 12:52:56 -0600

    socks post patch delete

--- api/socks.js
+++ api/socks.js
@@ -5,7 +5,37 @@ module.exports = socksRouter;
 socksRouter.get("/", async (req, res, next) => {
   try {
     res.send({
-      message: "Socks API up and runnning."
+      message: "Socks API up and runnning.",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+socksRouter.post("/", async (req, res, next) => {
+  try {
+    res.send({
+      message: "under construction",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+socksRouter.patch("/", async (req, res, next) => {
+  try {
+    res.send({
+      message: "under construction",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+socksRouter.delete("/", async (req, res, next) => {
+  try {
+    res.send({
+      message: "under construction",
     });
   } catch (error) {
     next(error);
