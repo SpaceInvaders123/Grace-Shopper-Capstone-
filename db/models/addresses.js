2557cfaaac3189b05cb1b16c3793cd3d77c83575
Hayden Smith <hayden.smith-25@outlook.com>
2022-02-19 12:19:29 -0600

    added updateAddresses adapter

--- db/models/addresses.js
+++ db/models/addresses.js
@@ -3,6 +3,7 @@ const client = require("../client");
 module.exports = {
   createAddresses,
   getAllAddresses,
+  updateAddresses,
 };
 
 async function createAddresses({ adress_line, state, city, zipcode }) {
@@ -28,6 +29,40 @@ async function getAllAddresses() {
     SELECT id, address_line, state, city, zipcode
     FROM addresses; `);
     return addresses;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function updateAddresses(addressesId, updateFields) {
+  try {
+    console.log(Object.keys(updateFields).length);
+
+    //removes any undefined fields from our API req.body
+    for (const key in updateFields) {
+      if (updateFields[key] === undefined) {
+        delete updateFields[key];
+      }
+    }
+
+    const setString = Object.keys(updateFields)
+      .map((key, idx) => `${key} = $${idx + 2}`)
+      .join(", ");
+
+    console.log(setString);
+
+    const {
+      rows: [address],
+    } = await client.query(
+      `
+    UPDATE addresses
+    SET ${setString}
+    WHERE id = $1
+    RETURNING *;`,
+      [addressesId, ...Object.values(updateFields)]
+    );
+
+    return address;
   } catch (err) {
     throw err;
   }
