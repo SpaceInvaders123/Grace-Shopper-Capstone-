ae5d9aa8690e0dd444ad93378ec7ac0dfa5e8fdc
shapirodaniel <shapirodanieladam@gmail.com>
2022-02-19 16:57:27 -0600

    hotfix: integration test logic, db client, yaml

--- db/client.js
+++ db/client.js
@@ -1,12 +1,28 @@
 // Connect to DB
-const { Client } = require("pg");
+const { Client } = require('pg');
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = "capstone-dev";
+const DB_NAME = 'capstone-dev';
 
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-const client = new Client(DB_URL);
+let client;
+
+// important! the client instance must be initialized with user/password
+// when running in the GitHub Actions CI environment
+if (process.env.CI) {
+  client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+  });
+  // otherwise, client is instantiated with the Heroku database url
+  // or the local devenv connection string
+} else {
+  client = new Client(DB_URL);
+}
 
 module.exports = client;
