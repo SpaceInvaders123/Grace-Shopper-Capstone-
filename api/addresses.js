4b89651cf9b02cc4f5415c55ff9da9a0a749ada8
Hayden Smith <hayden.smith-25@outlook.com>
2022-02-19 12:52:37 -0600

    get for addressesRouter, needs to be chekced

--- api/addresses.js
+++ api/addresses.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const addressesRouter = express.Router();
+const { Address } = require("../db/models");
 module.exports = addressesRouter;
 
 addressesRouter.get("/", async (req, res, next) => {
   try {
-    res.send({ message: "Addresses API up and running" });
+    const addresses = await Address.getAllAddresses();
+    res.send({ addresses });
   } catch (err) {
     next(err);
   }
