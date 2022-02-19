c954b8045ecb85da8a3b60681dc90b9c46e2e6d7
Hayden Smith <hayden.smith-25@outlook.com>
2022-02-18 20:02:57 -0600

    added addresses db adapter

---
+++ db/models/addresses.js
@@ -0,0 +1,22 @@
+const client = require("../client");
+
+module.exports = {
+  createAddresses,
+};
+
+async function createAddresses({ adress_line, state, city, zipcode }) {
+  try {
+    const {
+      rows: [adresses],
+    } = await client.query(
+      `
+    INSERT INTO adresses (adress_line, state, city, zipcode)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;`,
+      [adress_line, state, city, zipcode]
+    );
+    return adresses;
+  } catch (err) {
+    throw err;
+  }
+}
