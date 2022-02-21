92ae06bdd328b86bd0b57589229fa20d5c40456b
William Nollert <will.nollert@gmail.com>
2022-02-21 15:01:22 -0600

    adde createSocks route

--- api/socks.js
+++ api/socks.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const { getAllSocks } = require("../db/models/socks");
+const { getAllSocks, createSocks } = require("../db/models/socks");
 const socksRouter = express.Router();
 module.exports = socksRouter;
 
 socksRouter.get("/", async (req, res, next) => {
   try {
     const socks = await getAllSocks();
+    res.send(socks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+socksRouter.post("/", async (req, res, next) => {
+  try {
+    const { name, price, size, description, product_img, created_at } =
+      req.body;
+    const socks = await createSocks({
+      name,
+      price,
+      size,
+      description,
+      product_img,
+      created_at,
+    });
     res.send(socks);
   } catch (error) {
     next(error);
