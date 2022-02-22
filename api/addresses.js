f0f6a8ab89108e65d532422d5206b483fb9d2797
Hayden Smith <hayden.smith-25@outlook.com>
2022-02-21 19:39:42 -0600

    updated comment

--- api/addresses.js
+++ api/addresses.js
@@ -2,7 +2,7 @@ const express = require("express");
 const addressesRouter = express.Router();
 const { Address } = require("../db/models");
 module.exports = addressesRouter;
-//Route works now I resolved the issue
+//Route works now. I resolved the issue
 addressesRouter.get("/", async (req, res, next) => {
   try {
     const addresses = await Address.getAllAddresses();
