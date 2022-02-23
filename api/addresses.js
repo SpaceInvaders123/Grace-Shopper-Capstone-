63eab38281e2002372a5712039febf414399cbe7
Hayden Smith <hayden.smith-25@outlook.com>
2022-02-22 20:48:35 -0600

    all routes added for addresses. DO NOT MERGE MAIN. Need to check the newly added routes on postman - once thats done I will put in PR

--- api/addresses.js
+++ api/addresses.js
@@ -1,12 +1,59 @@
 const express = require("express");
 const addressesRouter = express.Router();
 const { Address } = require("../db/models");
+const authorizeUser = require("./auth");
 module.exports = addressesRouter;
 //Route works now. I resolved the issue
 addressesRouter.get("/", async (req, res, next) => {
   try {
     const addresses = await Address.getAllAddresses();
     res.send({ addresses });
+  } catch (err) {
+    next(err);
+  }
+});
+//Posts our newly created addresses
+addressesRouter.post("/", async (req, res, next) => {
+  try {
+    const { address_line, state, city, zipcode } = req.body;
+    const addresses = await Address.createAddresses({
+      address_line,
+      state,
+      city,
+      zipcode,
+    });
+    res.send(addresses);
+  } catch (err) {
+    next(err);
+  }
+});
+
+addressesRouter.patch(
+  "/:addressId",
+  [authorizeUser],
+  async (req, res, next) => {
+    try {
+      const { address_line, state, city, zipcode } = req.body;
+      const address = await Address.updateAddresses({
+        id: req.params.addressId,
+        address_line,
+        state,
+        city,
+        zipcode,
+      });
+      res.send(address);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+addressesRouter.delete("/:addressId", authorizeUser, async (req, res, next) => {
+  try {
+    const destroyAddress = await Address.hardDeleteAddresses(
+      req.params.addressId
+    );
+    res.send(destroyAddress);
   } catch (err) {
     next(err);
   }
