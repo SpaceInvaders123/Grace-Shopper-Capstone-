5a8758e1b62fe647513ab25a508cf20151288f56
Andrew Dake <andrewdake10@gmail.com>
2022-02-23 18:28:55 -0600

    added CRUDand db adapter functions

--- api/category.js
+++ api/category.js
@@ -1,12 +1,51 @@
 const express = require('express');
+const {
+  getAllCategories,
+  createCategory,
+  destroyCategory,
+  updateCategory,
+} = require("../db/models/category");
 const categoryRouter = express.Router();
 module.exports = categoryRouter;
 
 categoryRouter.get('/', async (req, res, next) => {
   try {
-    res.send({
-      message: 'Sock_category API up and runnning.',
+    const category = await getAllCategories();
+    res.send(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+categoryRouter.post("/", async (req, res, next) => {
+  try {
+    const { style } = req.body;
+    const category = await createCategory({
+      style
     });
+    res.send(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+categoryRouter.delete("/:categoryId", async (req, res, next) => {
+  try {
+    const destroy_category = await destroyCategory(req.params.categoryId);
+    res.send(destroy_category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+categoryRouter.patch("/:categoryId", async (req, res, next) => {
+  try {
+    const {style} = req.body;
+    const category = await updateCategory({
+      id: req.params.categoryId,
+      style,
+    });
+    res.send(category);
   } catch (error) {
     next(error);
   }
--- db/models/category.js
+++ db/models/category.js
@@ -2,6 +2,9 @@ const client = require('../client');
 
 module.exports = {
   createCategory,
+  getAllCategories,
+  destroyCategory,
+  updateCategory,
 };
 
 async function createCategory({ style }) {
@@ -13,11 +16,63 @@ async function createCategory({ style }) {
         INSERT INTO category (style)
         VALUES ($1)
         RETURNING *;
-        `,
+      `,
       [style]
     );
     return category;
   } catch (error) {
     throw error;
+  }
+}
+
+async function getAllCategories() {
+  try {
+    const {
+      rows: [category],
+    } = await client.query(
+      `
+        SELECT * FROM category;
+      `);
+
+    return category;
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function destroyCategory(categoryId) {
+  try {
+    const {
+      rows: [category],
+    } = await client.query(
+      `
+        DELETE FROM category
+        WHERE id=$1
+        RETURNING *;
+      `,
+      [categoryId]
+    );
+    return category;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function updateCategory({id, style}) {
+  try {
+    const {
+      rows: [category],
+    } = await client.query(
+      `
+        UPDATE category
+        SET style=$1,
+        WHERE id=$2
+        RETURNING *;
+      `,
+      [style, id]
+    );
+    return category;
+  } catch (err) {
+    throw err;
   }
 }
