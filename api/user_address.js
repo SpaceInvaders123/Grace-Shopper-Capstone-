bafaa239f33ffe537aa086dc37f4dc4288e04828
AMISHZAVER <51846056+ZVRAJ@users.noreply.github.com>
2022-02-23 21:00:54 -0600

    userAddressRouter.post

--- api/user_address.js
+++ api/user_address.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require("express");
 const userAddressRouter = express.Router();
 module.exports = userAddressRouter;
 
-userAddressRouter.get('/', async (req, res, next) => {
+userAddressRouter.get("/", async (req, res, next) => {
   try {
-    res.send({ message: 'User_address API up and running' });
+    res.send({ message: "User_address API up and running" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+userAddressRouter.post("/", async (req, res, next) => {
+  try {
+    const { addresses_id, created_at } = req.body;
+    const userAddress = await createUserAddress({
+      addresses_id,
+      created_at,
+    });
+    res.send(userAddress);
   } catch (err) {
     next(err);
   }
