84dbf9ca0a9e470fa11be74b141beb59e106bb48
William Nollert <will.nollert@gmail.com>
2022-03-07 15:59:57 -0600

    comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { App } from "./components";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
 import "./style/index.css";
+
+/* Will's changes; made two small changes here.
+
+1) Use BrowserRouter as our router. 
+This uses the browser’s History API to create real URLs 
+
+2) Use the Router to render our App component. 
+This will allow us to create the routes we need inside our App component. */
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
