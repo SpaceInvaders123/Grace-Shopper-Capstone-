937c879de93d724d3ed13abc0d4da29c8ffa4a53
Hayden Smith <hayden.smith-25@outlook.com>
2022-03-09 20:54:35 -0600

    updated formatter

--- src/components/SockCard.js
+++ src/components/SockCard.js
@@ -3,16 +3,22 @@ import "../style/SockCard.css";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 
-function formatPrice(price) {
-  return (price / 100).toFixed(2);
-}
+// function formatPrice(price) {
+//   return (price / 100).toFixed(2);
+// }
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+});
 
 const SockCard = ({ sock }) => {
   return (
     <Link to={`/socks/${sock.id}`}>
       <Card className="containercard">
         <Card.Title>{sock.name}</Card.Title>
-        <div className="card-sockprice">${formatPrice(+sock.price)}</div>
+        <div className="card-sockprice">{formatter.format(sock.price)}</div>
         <Card.Img variant="top" src={sock.product_img} alt={sock.name} />
         <Card.Text>{sock.description}</Card.Text>
       </Card>
