6081aea1dad851658e5231df722a31933a69b029
AMISHZAVER <51846056+ZVRAJ@users.noreply.github.com>
2022-03-09 18:29:47 -0600

    integrated footer, improved cards, implemented bootstrap, added logo

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,7 @@ const App = () => {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand href="/" className="font-weight-bold text-muted">
-          Socks4u
-        </Navbar.Brand>
+        <img href="./" src={require("../style/socks4you.png")} height="50" />
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav activeKey={window.location.pathname}></Nav>
